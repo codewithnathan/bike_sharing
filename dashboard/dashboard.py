ec906a642da5a457dd91c88990df53e7b8e83b8f
devswithme <developswithme@gmail.com>
2024-11-23 06:33:25 +0800

    init

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,17 +13,13 @@ def create_df_weather(df, selected_weathersit=None):
     if selected_weathersit is not None:
         df = df[df['weathersit'] == selected_weathersit]
     return df.groupby(['weathersit']).agg({
-        'temp': 'mean',
-        'atemp': 'mean',
-        'hum': 'mean',
-        'windspeed': 'mean'
+        'cnt': 'sum'
     }).reset_index()
-
 
 def create_df_year(df, selected_year=None):
     if selected_year is not None:
         df = df[df['yr'] == selected_year]
-    return df.groupby(['yr', 'season']).agg({
+    return df.groupby(['yr']).agg({
         'casual': 'sum',
         'registered': 'sum',
         'cnt': 'sum'
@@ -42,12 +38,12 @@ selected_weathersit = st.selectbox(
 
 day_df_weather = create_df_weather(day_df, selected_weathersit)
 
-st.header("Average Environment Condition Metrics by Weather Situation")
+st.header("Keadaan Lingkungan terhadap jumlah user")
 
 if not day_df_weather.empty:
     day_df_weather['weathersit'] = day_df_weather['weathersit'].map(weathersit_mapping)
     fig, ax = plt.subplots(figsize=(10, 6))
-    day_df_weather.plot(kind='bar', x='weathersit', y=['temp', 'atemp', 'hum', 'windspeed'], ax=ax)
+    day_df_weather.plot(kind='bar', x='weathersit', y=['cnt'], ax=ax)
     ax.set_ylabel('Average Value')
     ax.set_xlabel('Weather Situation')
     plt.xticks(rotation=0)
@@ -64,17 +60,11 @@ selected_year = st.selectbox(
 
 day_df_year = create_df_year(day_df, selected_year)
 
-st.header("Total Type User Count by Season per Year")
+st.header("Jumlah detail user setiap tahunnya")
 if not day_df_year.empty:
-    day_df_year['season'] = day_df_year['season'].map({
-        1: 'springer', 
-        2: 'summer',
-        3: 'fall',
-        4: 'winter'
-    })
     day_df_year['yr'] = day_df_year['yr'] + 1 
     fig, ax = plt.subplots(figsize=(10, 6))
-    day_df_year.plot(kind='bar', x='season', y=['cnt', 'registered', 'casual'], ax=ax, stacked=True)
+    day_df_year.plot(kind='bar', x='yr', y=['cnt', 'registered', 'casual'], ax=ax, stacked=True)
     ax.set_ylabel('Total User Count')
     ax.set_xlabel('Season')
     plt.xticks(rotation=0)
