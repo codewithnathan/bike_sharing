bf2752602a8b20d6536f69379bde4e6e4f9a1e7e
devswithme <developswithme@gmail.com>
2024-11-22 14:56:48 +0800

    first

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,100 +1,41 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
+import matplotlib.pyplot as plt
 
-sns.set(style='dark')
-
-def create_daily_users_df(df):
-    df['dteday'] = pd.to_datetime(df['dteday'])
-    return df.groupby(
-        pd.Grouper(key='dteday', freq='6MS')  # Group by 6-month periods
-    ).agg({
-        "registered_daily": "mean",
-        "cnt_daily": "mean",
-        "casual_daily": "mean"
+def create_df_weather(df):
+    return df.groupby(['weathersit']).agg({
+        'temp': 'mean',
+        'atemp': 'mean',
+        'hum': 'mean',
+        'windspeed': 'mean'
     }).reset_index()
 
-def create_hourly_users_df(df):
-    df['dteday'] = pd.to_datetime(df['dteday'])
-    return df.groupby(
-        pd.Grouper(key='dteday', freq='6MS')  # Group by 6-month periods
-    ).agg({
-        "registered_hourly": "mean",
-        "cnt_hourly": "mean",
-        "casual_hourly": "mean"
+def create_df_year(df):
+    return df.groupby(by='yr').agg({
+        'casual': 'sum',
+        'registered': 'sum',
+        'cnt': 'sum',
     }).reset_index()
 
-def create_seasonal_daily_users_df(df):
-    return df.groupby(by="season_daily").agg({
-    "casual_daily": "sum",
-    "registered_daily": "sum",
-    "cnt_daily":  "sum"
-}).reset_index()
+day_df = pd.read_csv('data/day.csv')
 
-def create_seasonal_hourly_users_df(df):
-    return df.groupby(by="season_hourly").agg({
-    "casual_hourly": "sum",
-    "registered_hourly": "sum",
-    "cnt_hourly":  "sum"
-}).reset_index()
+day_df_weather = create_df_weather(day_df)
+day_df_year = create_df_year(day_df)
 
-all_df = pd.read_csv('dashboard/main_data.csv')
+st.title("Bike Sharing Data Analysis")
 
-daily_users_df = create_daily_users_df(all_df)
-hourly_users_df = create_hourly_users_df(all_df)
-seasonal_daily_users_df = create_seasonal_daily_users_df(all_df)
-seasonal_hourly_users_df = create_seasonal_hourly_users_df(all_df)
+st.header("Average Environment Condition Metrics by Weather Situation")
+fig, ax = plt.subplots(figsize=(10, 6))
+day_df_weather.plot(kind='bar', x='weathersit', y=['temp', 'atemp', 'hum', 'windspeed'], ax=ax)
+ax.set_ylabel('Average Value')
+ax.set_xlabel('Weather Situation')
+plt.xticks(rotation=0)
+st.pyplot(fig)
 
-st.header('Bike Sharing Collection Dashboard')
-tab1, tab2 = st.tabs(["Users", "Season"])
-
-with tab1:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(daily_users_df['dteday'], daily_users_df['registered_daily'], label='Registered Daily', marker='o',)
-    ax.plot(daily_users_df['dteday'], daily_users_df['cnt_daily'], label='Total Daily', marker='o',)
-    ax.plot(daily_users_df['dteday'], daily_users_df['casual_daily'], label='Casual Daily', marker='o',)
-
-    ax.set_title('Average user type per 6 month daily in 2011 - 2012')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Users')
-    ax.legend()
-
-    st.pyplot(fig)
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(hourly_users_df['dteday'], hourly_users_df['registered_hourly'], label='Registered Hourly', marker='o',)
-    ax.plot(hourly_users_df['dteday'], hourly_users_df['cnt_hourly'], label='Total Hourly', marker='o',)
-    ax.plot(hourly_users_df['dteday'], hourly_users_df['casual_hourly'], label='Casual Hourly', marker='o',)
-
-    ax.set_title('Average user type per 6 month hourly in 2011 - 2012')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Users')
-    ax.legend()
-
-    st.pyplot(fig)
-
-with tab2:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.bar(seasonal_daily_users_df['season_daily'], seasonal_daily_users_df['cnt_daily'], color='skyblue', label='Casual Daily Users')
-
-    ax.set_title('Total user per season daily in 2011 - 2012')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Users')
-    ax.legend()
-
-    plt.xticks(rotation=45)
-
-    st.pyplot(fig)
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.bar(seasonal_hourly_users_df['season_hourly'], seasonal_hourly_users_df['cnt_hourly'], color='skyblue', label='Casual Hourly Users')
-
-    ax.set_title('Total user per season hourly in 2011 - 2012')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Users')
-    ax.legend()
-
-    plt.xticks(rotation=45)
-
-    st.pyplot(fig)
+st.header("Total Type User Count by Year")
+fig, ax = plt.subplots(figsize=(10, 6))
+day_df_year.plot(kind='bar', x='yr', y=['cnt', 'registered', 'casual'], ax=ax, stacked=True)
+ax.set_ylabel('Total User Count')
+ax.set_xlabel('Year')
+plt.xticks(rotation=0)
+st.pyplot(fig)
